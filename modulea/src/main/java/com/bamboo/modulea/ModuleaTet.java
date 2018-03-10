87892abcca4bb1e8dcb6ecf19ac2818c058d5b45
唐帅 <tangshuai@talkweb.com.cn>
2018-03-10 12:55:42 +0800

    修改Modulea的构建脚本

---
+++ modulea/src/main/java/com/bamboo/modulea/ModuleaTet.java
@@ -0,0 +1,13 @@
+package com.bamboo.modulea;
+
+import com.bamboo.component.MainActivity;
+
+/**
+ * Created by tangshuai on 2018/3/10.
+ */
+
+public class ModuleaTet {
+    public static void main(String[] args) {
+        MainActivity mainActivity;
+    }
+}
